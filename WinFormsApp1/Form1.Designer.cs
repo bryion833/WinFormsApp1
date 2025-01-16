76461290d74f0e4a22c86801f008ea783c03f633
bryion zeller <bryion.zeller@student.centralia.edu>
2025-01-16 10:16:03 -0800

    added form into todo list

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,31 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            groupBox2 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // groupBox1
             // 
-            button1.Location = new Point(290, 169);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(506, 119);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "taskform";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(94, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(406, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(94, 51);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "2025-01-16";
+            textBox2.Size = new Size(406, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Task";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Due Date";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(425, 80);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(75, 23);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "Add Todo";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(345, 80);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(74, 23);
+            textBox4.TabIndex = 5;
+            textBox4.Text = "Clear";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(12, 137);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(506, 671);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Task List";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(530, 820);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private GroupBox groupBox2;
     }
 }
---
+++ WinFormsApp1/to do.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class ToDo() 
+    internal class to_do
+    {
+        public static int ID = 0;
+        public int MyProperty { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Isdone { get; set; }
+
+        public ToDo(string description, DateTime Date, bool doneState)
+    }
+    objectCounter++
+        ID= ObjectCounter;
+        TaskDescription = DescriptionAttribute;
+        DueDate = DateTime;
+        isdone = doneState;
+
+}
